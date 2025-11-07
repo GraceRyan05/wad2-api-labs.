b73b00b29655c2f382afdb57dfaeac42e20e55d7
Grace <20106694@mail.wit.ie>
2025-11-07 09:24:07 +0000

    Completed node lab 1!

---
+++ node_lab1/greeting.js
@@ -0,0 +1,12 @@
+const greeting = {
+    en: "Hello World!",
+    fr: "Bonjour!",
+    es: "¡Hola!",
+    de: "Hallo",
+    it: "Ciao!",
+    jp: "こんにちは",
+    cn: "你好",
+    kr: "안녕하세요",
+};
+
+export default greeting;
